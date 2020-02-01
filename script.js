3160843a9e6c4933ad945a7344ff9532baa647ed
Ozan <37782926+OzanBoran@users.noreply.github.com>
2020-02-01 04:41:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function createListElement(){
+    var li = document.createElement("li");
+    var btn= document.createElement("button");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        btn.appendChild(document.createTextNode("Delete"));
+        li.appendChild(btn);
+        input.value = "";
+        btn.onclick = removeParent;
+}
+
+function addListAfterClick(){
+    if(input.value.length > 0){
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event){
+    if(input.value.length > 0 && event.keyCode === 13){
+        createListElement();
+    }
+}
+
+function deleteListAfterClick(){
+    var list = document.querySelector("li");
+    list.parentNode.removeChild(list);
+}
+
+function removeParent(evt){
+	evt.target.parentNode.remove();
+}
+
+button.addEventListener("click", addListAfterClick)
+
+input.addEventListener("keypress", addListAfterKeypress);
+
